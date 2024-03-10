83ffe471319d87952740a19a5a7b6bacba7de32e
IslamGoher <islam.m.goher@gmail.com>
2024-03-10 04:09:14 +0200

    Add a better solution for the problem `347. Top K Frequent Elements`

--- 347. Top K Frequent Elements.ts
+++ 347. Top K Frequent Elements.ts
@@ -1,3 +1,5 @@
+/*
+// O(n log(n))
 function topKFrequent(nums: number[], k: number): number[] {
   const numsCount = {};
   const result: number[] = [];
@@ -16,4 +18,26 @@ function topKFrequent(nums: number[], k: number): number[] {
   }
 
   return result;
+};
+*/
+
+// O(n)
+function topKFrequent(nums: number[], k: number): number[] {
+    let result: number[] = [];
+    const frequency = new Array(nums.length);
+    const numsCount = {};
+    nums.forEach(el => {
+        numsCount[el] = (numsCount[el]) ? numsCount[el]+1 : 1;
+    });
+    for (const key in numsCount) {
+        const value = numsCount[key];
+        if (frequency[value]) frequency[value].push(parseInt(key))
+        else frequency[value] = [parseInt(key)];
+    }
+    for (let i = frequency.length; i > 0; i--) {
+        if (result.length === k) break;
+        if (frequency[i]) result = result.concat(frequency[i])
+    }
+
+    return result;
 };
